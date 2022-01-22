9f25ff9e212fd2d5d4b64ff5f3e5ee797d46f23b
levalves <levi.linux@hotmail.com>
2022-01-22 11:06:01 -0300

    teste

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,7 +6,7 @@ public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
 		Gato gato = new Gato();
-		Livro livro = new Livro();
+		Livro livro  = new Livro();
 		System.out.println(gato);
 		System.out.println(livro);
 
